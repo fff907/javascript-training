2d5a3a9dd554df1933e5e4e26d4f8e3e28daa0e1
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-02 15:40:16 +0900

    commit

--- 17_switch/main.js
+++ 17_switch/main.js
@@ -3,17 +3,18 @@
 const color = prompt('color?')
 
 switch (color) {
-  case 'red':
-    console.log('Stop!');
-    break;
-  case 'yellow':
-    console.log('Slow down!');
-    break;
-  case 'blue':
-  case 'green':
-    console.log('Go!');
-    break;
-  default:
-    console.log('Wrong color');
-    break;
+    case 'red':
+        console.log('Stop!');
+        break;
+    case 'yellow':
+        console.log('Slow down!');
+        break;
+    case 'blue':
+    case 'green':
+        console.log('Go!');
+        break;
+    default:
+        // どれにも当てはまらない場合
+        console.log('Wrong color');
+        break;
 }
