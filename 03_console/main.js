359bf916bdc39f79d18497288850f73da0c915f8
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-02 14:30:10 +0900

    commit

--- 03_console/main.js
+++ 03_console/main.js
@@ -19,7 +19,10 @@ console.log(price * 120 * rate);
 console.log(price * 130 * rate);
 console.log(price * 140 * rate);
 
-price = 151;
+// price = 151;
+// price = price + 1;
+// price += 1;　短く書ける。++でさらに短く (意味は同じ)
+price++;
 console.log(price * 120 * rate);
 console.log(price * 130 * rate);
 console.log(price * 140 * rate);
