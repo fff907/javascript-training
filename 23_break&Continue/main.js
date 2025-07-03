803d695e9d8bbe129abbce56e3301a070225e767
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-03 11:53:07 +0900

    commit

--- 23_break&Continue/main.js
+++ 23_break&Continue/main.js
@@ -7,9 +7,12 @@ for (let year = 1; year <= 30; year++) {
     if (amount < 200) {
         continue;
     }
+    // 200より小さい出力をカット
+    // continue：反復処理の途中でそれ以降の処理をスキップして、次の反復処理に移る
 
     if (amount > 300) {
         break;
     }
+    // 300より大きい出力をカット
     console.log(`Year ${year}:${amount}`);
 }
