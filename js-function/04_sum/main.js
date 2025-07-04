6ffe09b7bb18f6b71e0d89399a15cd610b386119
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-04 16:04:02 +0900

    commit

--- js-function/04_sum/main.js
+++ js-function/04_sum/main.js
@@ -7,3 +7,14 @@
 
     console.log(sum(300, 700));
 }
+
+{
+  function calculateTotal(price, amount, rate = 1.1) {
+    return price * amount * rate;
+  }
+
+  console.log(calculateTotal(100, 10));         // → 1100（rateは1.1）
+  console.log(calculateTotal(150, 10));         // → 1650（rateは1.1）
+  console.log(calculateTotal(200, 10));         // → 2200（rateは1.1）
+  console.log(calculateTotal(120, 10, 1.08));   // → 1296（rateを上書き）
+}
