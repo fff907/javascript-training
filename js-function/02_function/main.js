29d1b85b97b8479118177a1f567add8988e208a8
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-04 16:59:56 +0900

    commit

--- js-function/02_function/main.js
+++ js-function/02_function/main.js
@@ -8,3 +8,18 @@
     console.log(double(10));
     console.log(double(4) * 3);
 }
+
+// 仮引数`num`は関数の中でしか使えない（スコープの例）
+{
+  function double(num) {
+    return num * 2;
+  }
+
+  function triple(num) {
+    return num * 3;
+  }
+
+  console.log(double(10));
+  // console.log(num);
+  console.log(triple(20));
+}
