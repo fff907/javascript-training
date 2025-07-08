d1b614b7b0016b67eb393170324e919db6d9b962
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-08 16:00:06 +0900

    commit

---
+++ js-data-structure/08_forEach/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+    const scores = [
+        70,
+        90, 
+        80, 
+        85,
+    ];
+
+    scores.forEach((score, index) => {
+    console.log(`${index}: ${score}`);
+  });
+}
