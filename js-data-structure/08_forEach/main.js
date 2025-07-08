3a09d4344e3d5548c642cd1b1107fa05dad05a56
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-08 17:25:19 +0900

    commit

--- js-data-structure/08_forEach/main.js
+++ js-data-structure/08_forEach/main.js
@@ -7,8 +7,14 @@
         80, 
         85,
     ];
+    let sum = 0;
+    // 0で初期化しておくと、sumは数値だということが分かり、読みやすいコードになる。
+    // 値の再代入しているので、ここはconstではなくて、letにする必要がある。
 
     scores.forEach((score, index) => {
     console.log(`${index}: ${score}`);
   });
+
+  console.log(`Sum: ${sum}`);
+  console.log(`Average: ${sum / scores.length}`);
 }
