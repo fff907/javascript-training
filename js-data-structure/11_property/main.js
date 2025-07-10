0e1621acbe7c0ce37979b9701eb7cc1fa5e5973c
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-10 11:46:39 +0900

    commit

--- js-data-structure/11_property/main.js
+++ js-data-structure/11_property/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 {
-   const scores = {
-    math : 80,
-    english : 90,
-  };
-
-  console.log(scores);
+    const scores = {
+        math: 80,
+        english: 90,
+    };
+    // console.log(scores['english']);
+    console.log(scores.english);
+    // scores['math'] = 88;
+    scores.math = 88;
+    console.log(scores);
 }
