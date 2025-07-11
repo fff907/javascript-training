b1bb38ec45bfc1837da5556133129dab67d32f7a
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-11 11:48:44 +0900

    commit

--- js-data-structure/11_property/main.js
+++ js-data-structure/11_property/main.js
@@ -5,11 +5,14 @@
         math: 80,
         english: 90,
     };
-    // console.log(scores['english']);
     console.log(scores.english); //プロパティへのアクセス
     scores.math = 88; //プロパティへの代入(ドットで繋げる)
 
     scores.physics = 70; //プロパティの追加(新しく'物理学'を追加)
     delete scores.english; //プロパティの削除('delete'を使う)
     console.log(scores);
+
+    // 結果
+    // math: 88
+    // physics: 70
 }
