b8bde088c41c6c69a665ed7b8d44c63cb55f6ba7
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-13 19:52:29 +0900

    commit

--- js-function/13_object/main.js
+++ js-function/13_object/main.js
@@ -1,3 +1,10 @@
+// プロパティが増えた場合、配列の反復処理を使いたいところだが、
+// オブジェクトに対して直接forEach()を使うことはできない　
+// →　一旦オブジェクトを配列に変換してから forEach()を使うというテクニックがある 
+// →　Object.entries(scores);とすると、それぞれのプロパティを配列にして、
+// さらにそれらをまとめた配列を取得する事ができる。(詳細は以下)
+
+
 'use strict';
 
 {
@@ -7,9 +14,16 @@
   };
 
   const entries = Object.entries(scores); //Oは必ず大文字！
-  console.log(entries);
-//   entries.forEach((prop) => {
-//     console.log(prop);
-//     console.log(`${prop[0]}: ${prop[1]}`);
-//   });
+//   console.log(entries);
+//   結果は「(2) [Array(2), Array(2)]」
+// →　0: (2) ['math', 80]、1: (2) ['english', 90]、length: 2、と表示され、
+// 配列の中に2つ配列が入り、それぞれがキーと値の配列になる。
+// →　この外側の配列に対して forEach() を使えば配列を取り出せるので、
+// entries に対して forEach() として、アロー関数式を渡す。
+// そうすると、entries の中にあった配列を1つずつ引数にセットしてくれる。
+
+  entries.forEach((prop) => { //←プロパティの略で「prop」
+    console.log(prop);
+    console.log(`${prop[0]}: ${prop[1]}`);
+  });
 }
