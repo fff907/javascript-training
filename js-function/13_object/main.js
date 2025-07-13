e6fe73fded882f58d670b6e3fdef5ed6515d4abf
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-13 19:39:26 +0900

    commit

---
+++ js-function/13_object/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  const scores = {
+    math: 80,
+    english: 90,
+  };
+
+  const entries = Object.entries(scores); //Oは必ず大文字！
+  console.log(entries);
+//   entries.forEach((prop) => {
+//     console.log(prop);
+//     console.log(`${prop[0]}: ${prop[1]}`);
+//   });
+}
