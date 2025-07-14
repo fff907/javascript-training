62e4c8166717901d64e8a8fecd471e464ac84f58
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 11:15:35 +0900

    commit

--- js-function/13_object/main.js
+++ js-function/13_object/main.js
@@ -6,7 +6,7 @@
     english: 90,
   };
   let sum = 0; // 合計を保持するための変数をsumで宣言し、0で初期化
-  scores.physics = 70;
+  scores.physics = 70; // 物理学を追加
 
   // 今までのやり方：1つずつ取り出して表示（数が少ないとき）
   // console.log(scores.math);
@@ -21,12 +21,13 @@
   // entries.forEach(function(prop) { のところを短く見やすく。
   // (prop)＝プロパティ＝['math', 80],['english', 90],
 
-    sum += prop[1];
+    sum += prop[1]; // 値（点数）を1つずつ足す → 合計にする
 
     console.log(`${prop[0]}: ${prop[1]}`); // math: 80、english: 90で表示
     //prop[0] → 'math'（キー）、prop[1] → 80（値）
 
     console.log(`Sum: ${sum}`);
     console.log(`Average: ${sum / entries.length}`);
+    // 毎回、合計と平均を表示（平均＝合計 ÷ 科目数）
   });
 }
