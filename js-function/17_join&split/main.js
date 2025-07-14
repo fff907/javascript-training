5f8178f5910326504bb36abf4e2cc9d0019493a2
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 16:28:33 +0900

    commit

--- js-function/17_join&split/main.js
+++ js-function/17_join&split/main.js
@@ -8,4 +8,8 @@
   console.log(names.join()); // Taro,Jiro,Saburo
   // |でもカンマでも区切らず繋げたい時
   console.log(names.join(''));
+
+  // 逆に文字列から配列を作りたい時
+  const names = 'Taro|Jiro|Saburo';
+  console.log(names.split('|'));
 }
