031be181f285eb1ea7102c6cdcdc60f5f67c7090
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 11:01:18 +0900

    commit

--- js-function/13_object/main.js
+++ js-function/13_object/main.js
@@ -5,6 +5,8 @@
     math: 80,
     english: 90,
   };
+  let sum = 0; // 合計を保持するための変数をsumで宣言し、0で初期化
+  scores.physics = 70;
 
   // 今までのやり方：1つずつ取り出して表示（数が少ないとき）
   // console.log(scores.math);
@@ -15,7 +17,16 @@
   const entries = Object.entries(scores);
 
   entries.forEach((prop) => {
-  // 【復習：アロー関数】元々は entries.forEach(function(prop) { のところを短く見やすく。
-    console.log(`${prop[0]}: ${prop[1]}`); // math: 80 など
+  // 【復習：アロー関数】元々は
+  // entries.forEach(function(prop) { のところを短く見やすく。
+  // (prop)＝プロパティ＝['math', 80],['english', 90],
+
+    sum += prop[1];
+
+    console.log(`${prop[0]}: ${prop[1]}`); // math: 80、english: 90で表示
+    //prop[0] → 'math'（キー）、prop[1] → 80（値）
+
+    console.log(`Sum: ${sum}`);
+    console.log(`Average: ${sum / entries.length}`);
   });
 }
