7677f3ae2061de210e088460dcb3ae928f0583cb
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 16:39:20 +0900

    commit

--- js-function/17_join&split/main.js
+++ js-function/17_join&split/main.js
@@ -7,7 +7,7 @@
   // 要素をカンマで区切って表示させたい時
   // console.log(names.join()); // Taro,Jiro,Saburo
   // |でもカンマでも区切らず繋げたい時
-  // console.log(names.join(''));
+  // console.log(names.join('')); // TaroJiroSaburo
 
   // 逆に文字列から配列を作りたい時
   const names = 'Taro|Jiro|Saburo';
