2e1954f9497602f556871554b10220738cc8263e
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 16:24:30 +0900

    commit

--- js-function/17_join&split/main.js
+++ js-function/17_join&split/main.js
@@ -3,6 +3,9 @@
 {
   const names = ['Taro', 'Jiro', 'Saburo'];
   // 要素を｜で区切って表示させたい時
-  //  Taro|Jiro|Saburo
-  console.log(names.join('|'));
+  console.log(names.join('|')); // Taro|Jiro|Saburo
+  // 要素をカンマで区切って表示させたい時
+  console.log(names.join()); // Taro,Jiro,Saburo
+  // |でもカンマでも区切らず繋げたい時
+  console.log(names.join(''));
 }
