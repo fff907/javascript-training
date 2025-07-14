ff93e74bdd83b1315ecffaf38c80d4c93d98ff98
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-14 16:30:34 +0900

    commit

--- js-function/17_join&split/main.js
+++ js-function/17_join&split/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 {
-  const names = ['Taro', 'Jiro', 'Saburo'];
+  // const names = ['Taro', 'Jiro', 'Saburo'];
   // 要素を｜で区切って表示させたい時
-  console.log(names.join('|')); // Taro|Jiro|Saburo
+  // console.log(names.join('|')); // Taro|Jiro|Saburo
   // 要素をカンマで区切って表示させたい時
-  console.log(names.join()); // Taro,Jiro,Saburo
+  // console.log(names.join()); // Taro,Jiro,Saburo
   // |でもカンマでも区切らず繋げたい時
-  console.log(names.join(''));
+  // console.log(names.join(''));
 
   // 逆に文字列から配列を作りたい時
   const names = 'Taro|Jiro|Saburo';
-  console.log(names.split('|'));
+  console.log(names.split('|')); // 
 }
