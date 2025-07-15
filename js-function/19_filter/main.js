3051db8178c982e8f4247c922f735e8ce7736fd0
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-15 14:27:52 +0900

    commit

--- js-function/19_filter/main.js
+++ js-function/19_filter/main.js
@@ -2,5 +2,17 @@
 
 {
   const prices = [100, 150, 200];
-  
+  // 150円以上の価格だけを抽出し、これとは別の新しい配列を作りたい時
+  // const pricesOver150 = [];
+  // prices.forEach((price) => {
+  //   if (price >= 150) {
+  //     pricesOver150.push(price);
+  //   }
+  // }); //上記を簡潔に書くためにfilter()を使う
+
+  const pricesOver150 = prices.filter((price) => {
+    return price >= 150;
+  });
+
+  console.log(pricesOver150);
 }
