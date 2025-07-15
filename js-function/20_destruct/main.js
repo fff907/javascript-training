014598b66425eb3a50fc5a93fa2d97cd8235356c
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-15 16:08:23 +0900

    commit

--- js-function/20_destruct/main.js
+++ js-function/20_destruct/main.js
@@ -2,5 +2,16 @@
 
 {
   const scores = [70, 90, 80, 85];
-  
+  // これらの値をこの後のコードでよく使うことになったので、
+  // それぞれに分かりやすい定数名を付けたくなったとする。
+  const first = scores[0];
+  const second = scores[1];
+  const third = scores[2];
+  const fourth = scores[3];
+  // ↑これを一気にスッキリ書ける！！
+
+  console.log(first);
+  console.log(second);
+  console.log(third);
+  console.log(fourth);
 }
