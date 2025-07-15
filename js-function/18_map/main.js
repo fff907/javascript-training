1568dc12c1f6ffa406b0dee6a67aef55934343b6
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-15 11:16:32 +0900

    commit

--- js-function/18_map/main.js
+++ js-function/18_map/main.js
@@ -2,10 +2,14 @@
 
 {
   const prices = [100, 150, 200];
-  // それぞれを10%の税込み価格にした配列を別途用意する
-  const pricesWithTax = [];
-  prices.forEach((price) => {
-    pricesWithTax.push(price * 1.1);
+  // // それぞれを10%の税込み価格にした配列を別途用意する
+  // const pricesWithTax = [];
+  // prices.forEach((price) => {
+  //   pricesWithTax.push(price * 1.1);
+  // }); //上記を簡潔に書くためにmap()を使う
+
+  const pricesWithTax = prices.map((price) => {
+    return price * 1.1;
   });
 
   console.log(pricesWithTax);
