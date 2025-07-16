47ed2a927e66c3a210f891b4ae4349705055385a
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-16 16:55:11 +0900

    commit

---
+++ js-function/24_operation/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+  const moreScores = {
+    shiro: 77,
+    goro: 88,
+  };
+
+  const scores = {
+    taro: 80,
+    jiro: 70,
+    saburo: 90,
+    ...moreScores,
+  };
+  // const {taro, jiro, saburo} = scores;
+  const {taro, ...others} = scores; // {}は新しい配列でなく分割代入の記法(復習)
+
+  console.log(taro);
+  // console.log(jiro);
+  // console.log(saburo);
+  console.log(others);
+}
