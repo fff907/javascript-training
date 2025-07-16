4949c059e7b9ec84493cd5febefc412c721f6676
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-16 17:17:39 +0900

    commit

--- js-function/25_backup/main.js
+++ js-function/25_backup/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
 {
-  let num = 10;
-  const numBackup = num;
-  num = 99;
-  console.log(num); // 99
-  console.log(numBackup); // 10
+  // let num = 10;
+  // const numBackup = num;
+  // num = 99;
+  // console.log(num); // 99
+  // console.log(numBackup); // 10
+
+  // 上記処理を配列で行うと...
+  const nums = [10, 20, 30];
+  const numsBackup = nums;
+  nums[0] = 99;
+  console.log(nums);// [99, 20, 30]
+  console.log(numsBackup); // [10, 20, 30]
 }
