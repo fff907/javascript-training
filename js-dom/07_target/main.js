aeecaec30d583f7fd6f65d498789d69057867644
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-18 16:51:12 +0900

    commit

--- js-dom/07_target/main.js
+++ js-dom/07_target/main.js
@@ -6,6 +6,7 @@
       // 特定のリストだけ取得したい場合、
       // その項目のHTMLにクラス名(今回は.target)を付けて、
       // js側をquerySelectorAll('.target')とする。
+      // ※予備で付けたID('#second')のみ動かすことも可。
       li.textContent = 'Changed!';
     });
   });
