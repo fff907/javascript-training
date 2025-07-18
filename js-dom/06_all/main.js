51cfa4e889ce5fcb295684159b9878ad65c5c286
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-18 15:50:59 +0900

    commit

--- js-dom/06_all/main.js
+++ js-dom/06_all/main.js
@@ -2,6 +2,10 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-   document.querySelectorAll('li').textContent = 'Changed!';
+    document.querySelectorAll('li').forEach((li) => {
+      // 複数の要素を処理する場合(この場合はリストの3項目)、
+      // querySelectorAll()とforEach(反復処理)を組み合わせる必要がある
+      li.textContent = 'Changed!';
+    });
   });
 }
