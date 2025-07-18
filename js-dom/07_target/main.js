28e0df3fd34013ebc4f5c27901591c45c864e873
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-18 16:43:25 +0900

    commit

--- js-dom/07_target/main.js
+++ js-dom/07_target/main.js
@@ -3,7 +3,8 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     document.querySelectorAll('.target').forEach((li) => {
-      // 特定のリストだけ取得したい場合、その項目にクラス名(今回は.target)を付けて、
+      // 特定のリストだけ取得したい場合、
+      // その項目にクラス名(今回は.target)を付けて、
       // querySelectorAll('.target')とする。
       li.textContent = 'Changed!';
     });
