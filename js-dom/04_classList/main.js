f2c4941354e11a13eb8da9a591a0ae4f735eadec
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-18 12:00:11 +0900

    commit

--- js-dom/04_classList/main.js
+++ js-dom/04_classList/main.js
@@ -2,8 +2,11 @@
 
 {
     document.querySelector('button').addEventListener('click', () => {
-       document.querySelector('p').classList.add('pink-bg')
-    //  p要素を取得し、classList.addとする、
+       document.querySelector('p').classList.add('pink-bg', 'red-border');
+       document.querySelector('p').classList.remove('green-color');
+    //  p要素を取得し、クラスを追加するためにclassList.addとする。
     // ()内:p要素に追加したいクラス名 ※先頭の.(ドット)は要らない！！
+    // ,(カンマ)区切りで複数渡せる。
+    // 削除したいクラスはclassList.removeとする。
     });
 }
