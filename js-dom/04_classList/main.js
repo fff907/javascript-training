17577b4157e1774235841aa90a6131ef1181946c
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-18 11:54:14 +0900

    commit

--- js-dom/04_classList/main.js
+++ js-dom/04_classList/main.js
@@ -2,6 +2,8 @@
 
 {
     document.querySelector('button').addEventListener('click', () => {
-       
+       document.querySelector('p').classList.add('pink-bg')
+    //  p要素を取得し、classList.addとする、
+    // ()内:p要素に追加したいクラス名 ※先頭の.(ドット)は要らない！！
     });
 }
