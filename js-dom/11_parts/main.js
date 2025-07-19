b94ea1245c4a1def5a222376b22aa667f613f605
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-19 23:51:13 +0900

    commit

--- js-dom/11_parts/main.js
+++ js-dom/11_parts/main.js
@@ -2,10 +2,14 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    alert(document.querySelector('input').value);
+    // alert(document.querySelector('input').value);
     // ↑ textContentではない!!
     // フォーム部品の値にアクセスするにはvalueを使う!!
     // 結果：OKボタンを押すと、
     // テキストフォームに入力した内容のアラートが出せる。
+
+    document.querySelector('textarea').value = '';
+    // ''の部分＝空文字を代入→
+    // ボタンをクリックしたらtextareaの中の値がクリアされる
   });
 }
