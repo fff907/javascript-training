db17c1fc6455cad671555c1464213bae65d289c7
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-20 00:05:43 +0900

    commit

---
+++ js-dom/13_radio/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+  document.querySelector('button').addEventListener('click', () => {
+    document.querySelectorAll('input').forEach((radio) => {
+      if (radio.checked === true) {
+        alert(radio.value);
+      }
+    });
+  });
+}
