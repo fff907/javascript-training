24e644a5ca5ede712e1eec831c469dca027779f0
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-19 23:25:49 +0900

    commit

--- js-dom/08_create/main.js
+++ js-dom/08_create/main.js
@@ -10,16 +10,16 @@
 
     // ▼ 要素の追加パターン①：末尾に追加（appendChild）
     // 実行結果：Taro → Jiro → Saburo → Hanako（←いちばん最後に追加される）
-    document.querySelector('ul').appendChild(liElement);
+    // document.querySelector('ul').appendChild(liElement);
 
     // ▼ 要素の追加パターン②：指定した要素の「直前」に追加（insertBefore）
     // 実行結果：Taro → Hanako → Jiro → Saburo（←Jiroの前に割り込む）
     // 第1引数：追加したい要素（liElement）
     // 第2引数：目印となる要素（ここでは「Jiro」）
-    // document.querySelector('ul').insertBefore(
-    //   liElement,
-    //   document.querySelector('#second')
-    // );
+    document.querySelector('ul').insertBefore(
+      liElement,
+      document.querySelector('#second')
+    );
 
     // ▼ 要素の削除（確認ダイアログでOKを押すと「Jiro」が消える）
     // if (confirm('Sure?') === true) {
