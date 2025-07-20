163dc531f34977f9f32129da2611dbc84e6d0aa4
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-20 23:11:03 +0900

    commit

---
+++ js-dom/19_submit/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+{
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    document.querySelector('p').textContent = document.querySelector('input').value;
+  });
+}
