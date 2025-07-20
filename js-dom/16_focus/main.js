78afb39c740476d81b0ab3e1c84fb75265cfa172
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-20 19:48:15 +0900

    commit

--- js-dom/16_focus/main.js
+++ js-dom/16_focus/main.js
@@ -2,6 +2,15 @@
 
 {
   document.querySelector('input').addEventListener('focus', () => {
-    document.querySelector('p').textContent = 'English only!';
+    // フォーカスを当てた時の動作
+    document.querySelector('p').textContent = 'English only!'; // メッセージ表示
   });
+
+  document.querySelector('input').addEventListener('blur', () => {
+    // フォーカスを外した時の動作
+    document.querySelector('p').textContent = ''; // テキストフォームの文字を消す
+  });
+
+  // ページを読み込んだと同時にフォーカスを当てておく動作
+  document.querySelector('input').focus();
 }
