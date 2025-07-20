e9f73bb214dc41fcce7a2a9c8bdd6128fafc4ae9
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-21 01:05:49 +0900

    commit

---
+++ js-dom/21_style/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+{
+  document.querySelector('button').addEventListener('click', () => {
+    // document.querySelector('p').style = 'font-size: 24px';
+    document.querySelector('p').style.fontSize = '24px';
+  });
+}
