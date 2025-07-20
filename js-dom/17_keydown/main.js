7cdd6dac188d2f5ccab56b109b42c0659a1338ce
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-20 21:02:31 +0900

    commit

--- js-dom/17_keydown/main.js
+++ js-dom/17_keydown/main.js
@@ -2,6 +2,11 @@
 
 {
   document.addEventListener('keydown', (e) => {
+  // 文章全体に対してイベントリスナーを設定したい場合、
+  // `document`に対して直接 `addEventListener` する。
+  // 引数名は何でもよいが、一般的にイベントの`e`が使われる。
     document.querySelector('p').textContent = e.key;
+    // `p`要素を取得してから、その `textContent`に`e.key`とすれば
+    // 入力されたキーの値を表示できる。
   });
 }
