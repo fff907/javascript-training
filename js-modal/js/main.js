0e2c80844a3d91dd1d6a42806a4c833dc41403f3
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-23 11:54:11 +0900

    commit

--- js-modal/js/main.js
+++ js-modal/js/main.js
@@ -5,4 +5,16 @@
   const close = document.getElementById('close');
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
+
+  open.addEventListener ('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+    // open:'hidden'を'remove'=モーダルを表示？
+  });
+
+  close.addEventListener ('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+    // close:'hidden'を'add'=モーダルを非表示？
+  });
 }
