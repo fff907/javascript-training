aa41ab1304a2c13b99fb18075fde6198de300b85
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-24 14:08:38 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -10,11 +10,17 @@
     e.preventDefault();
     modal.classList.remove('hidden');
     mask.classList.remove('hidden');
+    // モーダルを開いたときに背景スクロールを止める
+    // ※body(背景全体)にoverflow: hiddenを指定
+    document.body.style.overflow = 'hidden';
   });
 
   close.addEventListener ('click', () => {
     modal.classList.add('hidden');
     mask.classList.add('hidden');
+    // モーダルを閉じたときにスクロールを元に戻す
+    // ※空文字 '' で元のスタイルに戻る
+    document.body.style.overflow = '';
   });
 
   mask.addEventListener('click', () => {
