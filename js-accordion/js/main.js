30f3dd50631cb3370ab73a255091e3aa445b8a77
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-25 16:21:32 +0900

    commit

--- js-accordion/js/main.js
+++ js-accordion/js/main.js
@@ -9,6 +9,12 @@
   dts.forEach(dt => {
     dt.addEventListener('click', () => {
       dt.parentNode.classList.toggle('appear');
+    // parentNode:
+    // その要素の“ひとつ上の親”を取得するプロパティ
+    // この .appear クラスが付いたら、
+    // その中の <dd> を display: blockで表示させる
+    // ＝クリックされた質問（<dt>）の答え（<dd>）を
+    // 表示・非表示に切り替える 
     });
   });
 }
