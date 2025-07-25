80a869e73bb90a63a08d1888b5b84693fdb4f427
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-25 14:17:36 +0900

    commit

--- js-hamburger/js/main.js
+++ js-hamburger/js/main.js
@@ -3,9 +3,15 @@
 {
   const open = document.querySelector('#open');
   const overlay = document.querySelector('.overlay');
+  const close = document.querySelector('#close');
 
   open.addEventListener('click', () => {
     overlay.classList.add('show');
     open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
   });
 }
