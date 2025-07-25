16b30090b0f170dfb58e55a5a7c132d36b37224d
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-25 16:03:50 +0900

    commit

--- js-accordion/js/main.js
+++ js-accordion/js/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
 {
-    
+  const dts = document.querySelectorAll('dt');
+  // dt要素にクリックイベントを設定したいので、
+  // 複数のdt要素という意味でdtsとして、 
+  // querySelectorAll を使う   
+
+  dts.forEach(dt => {
+    dt.addEventListener('click', () => {
+      dt.parentNode.classList.toggle('appear');
+    });
+  });
 }
