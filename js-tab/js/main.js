4d150f7c1e5f773545b11cd84ec22f381d38c4a1
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-28 17:20:04 +0900

    commit

--- js-tab/js/main.js
+++ js-tab/js/main.js
@@ -12,12 +12,12 @@
         item.classList.remove('active');
         // クリックしたメニュー項目のactiveクラスを外す 
         // 選択中のアイテムと混同しないよう'item'にする 
-      });
+       });
       clickedItem.classList.add('active');
       // クリックしたメニュー項目にactiveクラスをつける
       // 選択中のアイテムと混同しないよう
-      // 'clickedItem'にする   
-     });
+      // 'clickedItem'にする
+
      contents.forEach(content => {
         content.classList.remove('active');
       });
@@ -25,5 +25,9 @@
       // いったん全てのcontentに対して activeクラスを削除し,
       // クリックしたメニュー項目に応じて、
       // 対応したcontentにだけ activeクラスを付ける
+      // HTMLにid="about","service","contact"を付与(sectionタグ内)
+      // data-id="about""service","contact"も付与(ulタグ内)
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
   });
 }
