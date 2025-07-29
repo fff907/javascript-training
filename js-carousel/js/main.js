4fe546d9ea44020162c42bedd3ddec4ae656cd57
fff907 <59164817+fff907@users.noreply.github.com>
2025-07-29 14:32:44 +0900

    commit

--- js-carousel/js/main.js
+++ js-carousel/js/main.js
@@ -1,4 +1,10 @@
 'use strict';
 {
-    
+    const next = document.querySelector('next');
+    const ul = document.querySelector('ul');
+
+    next.addEventListener('click', () => {
+        ul.style.transform = 'translateX(-200px)';
+        // 左に200px動かす
+    });
 }
