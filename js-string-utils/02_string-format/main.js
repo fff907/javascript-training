3ad59431ef904a45105e7526d1ca419db83b0e9a
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-04 15:45:55 +0900

    commit

---
+++ js-string-utils/02_string-format/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  const string = prompt('Name?');
+  if (string.toUpperCase().trim() === 'TARO') {
+  // 大文字小文字を区別せずに文字列判定
+  // → 受け取った文字列をすべて大文字に変換してから'TARO'と比較
+  // ⇔ 小文字＝toLowerCase()
+  // .trim()＝前後の空白を除去
+  // (入力時うっかりスペースキーを押しても判定可能)
+    console.log('Correct!');
+  } else {
+    console.log('Wrong!');
+  }
+}
