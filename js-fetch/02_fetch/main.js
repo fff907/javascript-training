439dae2cb074c963b38153c6dcfdfd3b5484dfc6
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-05 16:43:02 +0900

    commit

--- js-fetch/02_fetch/main.js
+++ js-fetch/02_fetch/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
 {
-  
+  function showHeader () {
+    console.log('Header');
+  }
+
+  function showHeader () {
+    console.log('Users');
+  }
+
+  function showHeader () {
+    console.log('Footer');
+  }
+
+  showHeader();
+  showUsers();
+  showFooter();
 }
