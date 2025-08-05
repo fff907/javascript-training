13a9a261a786e8f4c3f0f4ed1dee3a06db2b86e1
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-05 13:49:54 +0900

    commit

--- js-string-utils/05_string-replace/main.js
+++ js-string-utils/05_string-replace/main.js
@@ -12,9 +12,13 @@
     // '@example.com'を''(空文字)に置換＝@以降を削除
     // ＝'taro''kintaro''kojiro'
 
-    // 文字列を配列化するsplitも効果的
     const items = email.split('@');
     console.log(items[0]);
+    // 文字列を配列化するsplitも効果的
+    // "@"を境界線として文字列を2つに分ける
+    // "taro@example.com" → ["taro", "example.com"]
+    // そのうちの items[0]（＝最初の要素）を表示
+    // → forEachでループしているため全要素表示
     // ＝'taro''kintaro''kojiro'
   });
 }
