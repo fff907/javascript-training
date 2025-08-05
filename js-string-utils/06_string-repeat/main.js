188a96ecfa8cbb6e1174008f848b0dbe7fb9bc30
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-05 14:52:42 +0900

    commit

--- js-string-utils/06_string-repeat/main.js
+++ js-string-utils/06_string-repeat/main.js
@@ -12,6 +12,11 @@
     // * を連結しグラフにする(配列内の数だけ * が出力される)
     // 上記処理を短く書くと、.repeat();
     const bar = '*'.repeat(count);
-    console.log(`${count}: ${bar}`);
+    // label部分の桁を揃えるために、
+    // pddStartを使って数字を左揃えする
+    // (揃えたい文字数, 文字詰めのための空白)
+    // ⇔ 右揃え＝pddEnd
+    const label = String(count).padStart(2, ' ');
+    console.log(`${label}: ${bar}`);
   });
 }
