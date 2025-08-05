94ee44fe178a774062d25d4debee64085036ed8a
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-05 17:12:57 +0900

    commit

--- js-fetch/02_fetch/main.js
+++ js-fetch/02_fetch/main.js
@@ -5,8 +5,19 @@
     console.log('Header');
   }
 
-  function showUsers () {
-    console.log('Users');
+  async function showUsers() {
+    const response = await fetch('https://dotinstall.github.io/setup/fetchapi/users.json');
+    const users = await response.json();
+    // console.log(response);
+    console.log(users);
+
+    // fetch()のみだと⇒非同期処理が開始した直後の値
+    // ＝Promise<pending>がコンソールに表示される
+    // 非同期処理が終わったあとの値を取得するには
+    // ⇒functionの前に'async'、fetchの前に'await'を付ける！
+    // ⇒結果、responseというオブジェクトが出力されるので、
+    // JSON 形式のデータを取り出すには、
+    // const users = await response.json();　を書く。
   }
 
   function showFooter () {
