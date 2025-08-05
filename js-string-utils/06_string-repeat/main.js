f1f97187cabc8a15e8d41d71e4b969766d86eb40
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-05 14:23:12 +0900

    commit

---
+++ js-string-utils/06_string-repeat/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+  const counts = [6, 12, 8, 15];
+
+  counts.forEach((count) => {
+    // let bar = '';　//  barを空文字で初期化
+    // for (let i = 0; i < count; i++) {
+    // iが0からcountに満たない間、
+    // iを1ずつ増やしながら次の処理をする
+    // bar = bar + '*'; }
+    // * を連結しグラフにする(配列内の数だけ * が出力される)
+    // 上記処理を短く書くと、.repeat();
+    const bar = '*'.repeat(count);
+    console.log(bar);
+  });
+}
