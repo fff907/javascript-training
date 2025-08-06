c816c22285e1894fe4a35e616c3c0c31024d4c01
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-07 08:55:47 +0900

    commit

--- js-datatype/02_boolean/main.js
+++ js-datatype/02_boolean/main.js
@@ -10,7 +10,7 @@
       pElement.textContent = 'Dev Mode is Off.';
       // ➀クリックしたらOff表示に！
       // isDevMode = !isDevMode;(クリックで真偽値反転！)
-      // "!="：否定の論理演算子
+      // `!="`：否定の論理演算子
       // (➀➁同じなので条件分岐の外に出しても同じ意味)
     } else {
       pElement.textContent = 'Dev Mode is On.';
