bd61fbd0bdcb0675c9bb3995381b01946f853998
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-06 17:19:30 +0900

    commit

--- js-datatype/02_boolean/main.js
+++ js-datatype/02_boolean/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
 {
+  let isDevMode = true;
+  const pElement = document.querySelector('p');
+  const buttonElement = document.querySelector('button');
   
+  buttonElement.addEventListener('click', () => {
+
+  });
 }
