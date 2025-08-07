7b75d109c02a0e7a93716fe823b1781b7547180c
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-07 10:06:24 +0900

    commit

--- js-datatype/03_branch/main.js
+++ js-datatype/03_branch/main.js
@@ -6,18 +6,13 @@
   const buttonElement = document.querySelector('button');
   
   buttonElement.addEventListener('click', () => {
-    if (isDevMode === true) {
-      pElement.textContent = 'Dev Mode is Off.';
-      // ➀クリックしたらOff表示に！
-      // isDevMode = !isDevMode;(クリックで真偽値反転！)
-      // `!=`：否定の論理演算子
-      // (➀➁同じなので条件分岐の外に出しても同じ意味)
-    } else {
-      pElement.textContent = 'Dev Mode is On.';
-      // ➁クリックしたらOn表示に！
-      // isDevMode = !isDevMode;(クリックで真偽値反転！)
-      // (➀➁同じなので条件分岐の外に出しても同じ意味)
+    if (confirm('Are you sure?')) { // 確認用ダイアログ表示
+      if (isDevMode) {
+        pElement.textContent = 'Dev Mode is Off.';
+      } else {
+        pElement.textContent = 'Dev Mode is On.';
+      }
+      isDevMode = !isDevMode;
     }
-    isDevMode = !isDevMode; // 条件分岐の外に出したもの
   });
 }
