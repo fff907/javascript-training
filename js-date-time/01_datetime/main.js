6aabe6a372a6ed4fc0a0c1a172cd5c95d473cd07
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-08 17:19:22 +0900

    commit

--- js-date-time/01_datetime/main.js
+++ js-date-time/01_datetime/main.js
@@ -2,6 +2,8 @@
 
 {
   const d = new Date();
-  console.log(d);
+  // new Date() = 現在の日時に関するデータをくれる  
+  console.log(d); // 現在の日時表示(Wed Sun 21...出力が長くややこしい)
   console.log(d.toLocaleString());
+  // toLocaleString()⇒スッキリとした表示にしてくれる yyyy/mm/dd XX:XX:xx  
 }
