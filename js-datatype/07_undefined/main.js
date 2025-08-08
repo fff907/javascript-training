10212bf00c2d7736a35c52d69eca0dc3b2f4df34
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-08 16:28:18 +0900

    commit

---
+++ js-datatype/07_undefined/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+  'use strict';
+
+  {
+    // undefinedが出るコード
+    // （関数にreturnを書かなかった時の返り値以外の例）
+    // undefinedは値が定義されているかどうかのチェックによく使われる
+
+    // let x;
+    // console.log(x);　宣言されただけの変数の値
+
+    // const scores = [70, 90, 80];
+    // console.log(scores[100]);
+    // 配列で存在しないインデックスの値にアクセスした時
+
+    const score = {math: 80, english: 90};
+    // console.log(score.history);
+    // オブジェクトで存在しないプロパティにアクセスした時
+    if (score.history === undefined) {
+      console.log('History score not defined!');
+    }
+  }
+}
