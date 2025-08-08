a6f66d3a77c56703666e103c88b692f8bf70c8d6
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-08 17:05:09 +0900

    commit

---
+++ js-datatype/09_typeof/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+  // typeof
+  // データ型を調べられる
+  console.log(typeof 5); // number
+  console.log(typeof 'Hello'); // string
+  console.log(typeof [5, 2]); // object
+  console.log(typeof {math: 80, english: 90}); // object
+  console.log(typeof true); // boolean
+  console.log(typeof undefined); // undefined
+  console.log(typeof null); // object←ここだけ注意！
+}
