bd19468829bb9a4e6ea525b20e69d7be24030b7b
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-11 17:40:45 +0900

    commit

--- js-date-time/02_specified/main.js
+++ js-date-time/02_specified/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 {
-  const d = new Date(2000, 3, 11);
+  // const d = new Date(2000, 3, 11);
   // 2000年4月11日を表示。2000/4/11 00:00:00
+  const d = new Date(2000, 2, 0);
+  console.log(d.toLocaleString());
 }
