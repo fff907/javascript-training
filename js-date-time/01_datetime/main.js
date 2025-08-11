b43345b6bbe5c7c768219be05a071d9b051d3212
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-11 17:20:26 +0900

    commit

--- js-date-time/01_datetime/main.js
+++ js-date-time/01_datetime/main.js
@@ -6,4 +6,12 @@
   console.log(d); // 現在の日時表示(Wed Sun 21...出力が長くややこしい)
   console.log(d.toLocaleString());
   // toLocaleString()⇒スッキリとした表示にしてくれる yyyy/mm/dd XX:XX:XX
+  console.log(d.getFullYear()); // 年を取得
+  console.log(d.getMonth()); // 月を取得。0, 1, 2, ...
+  console.log(d.getDate()); // 日付を取得
+  console.log(d.getHours()); // 時間を取得
+  console.log(d.getMinutes()); // 分を取得
+  console.log(d.getSeconds()); // 秒を取得
+  console.log(d.getMilliseconds()); // ミリ秒を取得
+  console.log(d.getDay()); // 曜日を取得。0, 1, 2, ...
 }
