ac4377ebaf543ded765995130f3a8f33140714eb
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-13 15:43:53 +0900

    commit

--- js-countdown/main.js
+++ js-countdown/main.js
@@ -3,4 +3,15 @@
 {
   const timer = document.querySelector('#timer');
   const btn = document.querySelector('#btn');
+
+  // (1) 終了時刻を求める
+  btn.addEventListener('click', ()=> {
+    const endTime = new Date().getTime() + 3 * 1000; 
+    // new Date(現在の日時を求める)
+    // ＋getTime(基準日から経過ミリ秒を表す整数値に変換後 計算)
+    // ＋カウントダウンの時間分 * 1000(ミリ秒に単位を揃える)
+  });
+
+  // (2) 残り時間を求める
+  // (3) タイマーの終了
 }
