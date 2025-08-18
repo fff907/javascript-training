ccf8783a7ebca587eb598946e917908531cb8ffd
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-18 17:56:19 +0900

    commit

--- js-three-choice/main.js
+++ js-three-choice/main.js
@@ -3,8 +3,15 @@
 {
   function render(quiz) {
     const main = document.querySelector('main');
+
     const section = document.createElement('section');
+    const h2 = document.createElement('h2');
+    h2.textContent = quiz[0];
+
+    section.appendChild(h2);
+    // HTMLの`<section>`タグ内に`<h2>1の正解は？</h2>`が生成される
     main.appendChild(section);
+    // HTMLの`</main>`の直前に`<section></section>`が生成される
   }
 
   // 問題文, 選択肢, 選択肢, 選択肢, 正解(0, 1, 2)
