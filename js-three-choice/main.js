34eb697ab9c04f7986094909a002579a7c761bb3
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-18 17:38:42 +0900

    commit

--- js-three-choice/main.js
+++ js-three-choice/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
 {
+  function render(quiz) {
+    const main = document.querySelector('main');
+    const section = document.createElement('section');
+    main.appendChild(section);
+  }
+
   // 問題文, 選択肢, 選択肢, 選択肢, 正解(0, 1, 2)
   const quiz = ['1の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 0];
+
+  render(quiz);
 }
