8ef592076c8348fdc4f182633e3480655b4a6f19
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-19 10:37:16 +0900

    commit

--- js-three-choice/main.js
+++ js-three-choice/main.js
@@ -8,10 +8,16 @@
     const h2 = document.createElement('h2');
     h2.textContent = quiz[0];
 
+    const ul = document.createElement('ul');
+
     section.appendChild(h2);
     // ➁下記`main.appendChild(section);`で生成した
     // `<section></section>`タグ内に
     // `<h2>1の正解は？</h2>`が生成される
+    section.appendChild(ul);
+    // ➂下記`main.appendChild(section);`で生成した
+    // `<section></section>`タグ内、
+    // `<h2>1の正解は？</h2>`の次に`<ul></ul>`が生成される
     main.appendChild(section);
     // ➀HTMLの`</main>`の直前に`<section></section>`が生成される
   }
