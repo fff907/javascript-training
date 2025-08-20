7163cbc4ceff014806d095c1173701e97f9d9aed
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-20 16:57:55 +0900

    commit

--- js-memo/main.js
+++ js-memo/main.js
@@ -3,7 +3,14 @@
 {
   const text = document.querySelector('#text');
   const save = document.querySelector('#save');
+  const clear = document.querySelector('#clear');
   const message = document.querySelector('#message');
+
+  if (localStorage.getItem('memo') === null) {
+    text.value = '';
+  } else {
+    text.value = localStorage.getItem('memo');
+  }
 
   save.addEventListener('click', () => {
     message.classList.add('appear');
@@ -11,5 +18,12 @@
       message.classList.remove('appear');
     }, 1000);
     localStorage.setItem('memo', text.value);
+  });
+
+  clear.addEventListener('click', () => {
+    if (confirm('本当に削除しますか？') === true) {
+      text.value = '';
+      localStorage.removeItem('memo');
+    }
   });
 }
