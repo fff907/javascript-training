63a9ce72718a905d09df616ec00ee3eaefd77a1e
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-20 16:04:57 +0900

    commit

--- js-memo/main.js
+++ js-memo/main.js
@@ -3,6 +3,7 @@
 {
   const text = document.querySelector('#text');
   const save = document.querySelector('#save');
+  const message = document.querySelector('#message');
 
   save.addEventListener('click', () => {
     localStorage.setItem('memo', text.value);
