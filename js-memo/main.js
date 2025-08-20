3c1063aef1d412d2b894b784707e0e36e970974e
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-20 16:24:18 +0900

    commit

--- js-memo/main.js
+++ js-memo/main.js
@@ -1,11 +1,15 @@
 'use strict';
 
 {
-  const text = document.querySelector('#text');
-  const save = document.querySelector('#save');
-  const message = document.querySelector('#message');
+  const text = document.getElementById('text');
+  const save = document.getElementById('save');
+  const message = document.getElementById('message');
 
   save.addEventListener('click', () => {
+    message.classList.add('appear');
+    setTimeout(() => {
+      message.classList.remove('appear');
+    }, 1000);
     localStorage.setItem('memo', text.value);
   });
 }
