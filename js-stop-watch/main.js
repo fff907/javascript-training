4b4b5e1a36df21c6477eca0593605f204419c87c
fff907 <59164817+fff907@users.noreply.github.com>
2025-08-21 16:54:47 +0900

    commit

--- js-stop-watch/main.js
+++ js-stop-watch/main.js
@@ -9,10 +9,15 @@
 
   function countUp() {
     const d = new Date(Date.now() - startTime);
+    // dateのd。経過ミリ秒から開始時間を引く。
     const m = String(d.getMinutes()).padStart(2, '0');
+    // minuteのm。padStartで2桁表示に。文字列用の関数なのでStringで囲んでおく
     const s = String(d.getSeconds()).padStart(2, '0');
+    // secondのs。padStartで2桁表示に。文字列用の関数なのでStringで囲んでおく
     const ms = String(d.getMilliseconds()).padStart(3, '0');
+    // ミリ秒のms。padStartで3桁表示に。文字列用の関数なのでStringで囲んでおく
     timer.textContent = `${m}:${s}.${ms}`;
+    // テンプレートリテラルで00:00:000表示にする
 
     setTimeout(() => {
       countUp();
